b70e1346a0c6d1014b56be31c12ae5039b6e1024
AarkayITsolutions <alihassanali119683@gmail.com>
2025-02-25 09:18:31 +0500

    adding files

---
+++ src/main/java/com/hospital/MedicalRecordAPI/MedicalRecordApiApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.MedicalRecordAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRecordApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRecordApiApplication.class, args);
+	}
+
+}
