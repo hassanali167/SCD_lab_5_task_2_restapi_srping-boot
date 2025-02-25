3d2ce21d5c339ef945889e42594db92fd8de4082
AarkayITsolutions <alihassanali119683@gmail.com>
2025-02-25 09:20:02 +0500

    adding files

---
+++ src/test/java/com/hospital/MedicalRecordAPI/MedicalRecordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.MedicalRecordAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecordApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
